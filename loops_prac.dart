9eb4d94d138e304af6c4bb570aef0d5dcc01ecd9
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-11-20 17:14:31 +0500

    Added example code of Loops

---
+++ loops_prac.dart
@@ -0,0 +1,16 @@
+main() {
+  // for (int a = 1; a <= 10; a++) {
+  //   print("Hello World");
+  // }
+
+  int no = 10;
+
+  // do {
+  //   print("No is $no");
+  // } while (no < 50);
+
+  while (no <= 50) {
+    print("No is $no");
+    no++;
+  }
+}
