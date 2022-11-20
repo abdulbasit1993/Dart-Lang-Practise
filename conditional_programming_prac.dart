abfe1a37c7a20b7b78a64dbfe6417e10f137caf7
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-11-20 16:15:47 +0500

    Added example code of conditional programming

---
+++ conditional_programming_prac.dart
@@ -0,0 +1,34 @@
+void main() {
+  var a = 500;
+  var b = 50;
+
+  if (a > 200 || b < 10) {
+    print('Block 1');
+  } else {
+    print('Block 2');
+  }
+
+  // if (a > 200 && b > 100) {
+  //   // Cond 1 and Cond 2 are true
+  //   print("Block 1");
+  //   // when both if are true
+  // } else if (a < 50) {
+  //   // Cond 2 is true
+  //   print("Block 2");
+  // } else if (a == 80) {
+  //   // Cond 3 is true
+  //   print("Block 3");
+  // } else if (a == 500) {
+  //   // Cond 4 is true
+  //   print("Block 4");
+  // } else {
+  //   // all Conds are false
+  //   print("Block ELSE");
+  // }
+
+  // if (a > 200) {
+  //   //true
+  // } else {
+  //   //false
+  // }
+}
