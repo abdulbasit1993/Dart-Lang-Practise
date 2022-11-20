a196b226dca5de61989293c9b36f544caf4e0522
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-11-20 15:44:39 +0500

    Added example code of final vs const keywords differences

---
+++ final_vs_const_prac.dart
@@ -0,0 +1,19 @@
+// Final vs Const Keywords - Difference and How to Use?
+
+void main() {
+  print('Welcome to Dart!');
+
+  final names = ["Abdul Basit", "Uzair", "Tariq", "Imran"];
+
+  names.add("Peter");
+
+  // (when using 'const' keyword we cannot add elements to a list, but with 'final' keyword we can add elements to the list.)
+
+  // names = ["Babar", "Ali", "Shahid", "Umair"];
+
+  // const name = "Abdul Basit"; // const variables must be initialized
+
+  // final String name;
+
+  // name = "Abdul Basit";
+}
